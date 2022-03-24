32894b61d3f5fa39f7782d43425b9e4964228f3e
Rudy Ramirez <77511166+rlramire10@users.noreply.github.com>
2022-03-23 19:38:40 -0500

    Add files via upload

---
+++ 2022 Programs/Information Security Encryption Exercises/AES Encryption Exercise/Alice.java
@@ -0,0 +1,106 @@
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileWriter;
+
+import javax.crypto.Cipher;
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.IvParameterSpec;
+import java.security.SecureRandom;
+import java.util.Base64;
+
+
+public class Alice{
+    //Initialize Global Variables
+	private SecretKey Key;
+    private IvParameterSpec IV;
+	private final int KEY_SIZE = 128;
+    private Cipher encryptionCipher;
+	
+	
+	//Initialize AES Class
+    public void init() throws Exception {
+        //Generate Key
+		KeyGenerator key_generator = KeyGenerator.getInstance("AES");
+        key_generator.init(KEY_SIZE);
+        Key = key_generator.generateKey();
+		
+		//Generate IV
+		byte[] iv = new byte[16];
+		new SecureRandom().nextBytes(iv);
+		IV = new IvParameterSpec(iv);
+    }
+	
+	public SecretKey getKey() {
+		return Key;
+	}
+	
+	public IvParameterSpec getIV() {
+		return IV;
+	}
+	
+	//Encryption
+	//**********************************************************************************
+    public String encrypt(String message, SecretKey key, IvParameterSpec iv) throws Exception {
+        //Create byte array of plaintext
+		byte[] messageInBytes = message.getBytes();
+		
+		//Create AES encryption cipher
+        encryptionCipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+        
+		//Initialize AES encryption cipher with generated key and IV
+		encryptionCipher.init(Cipher.ENCRYPT_MODE, key, iv);
+        
+		//Create encrypted message
+		byte[] encryptedBytes = encryptionCipher.doFinal(messageInBytes);
+        
+		//Return encrypted message
+		return encode(encryptedBytes);
+    }
+	
+	//Convert encrypted message from byte array to String
+	private String encode(byte[] data) {
+        return Base64.getEncoder().encodeToString(data);
+    }
+	//**********************************************************************************
+
+    public static void main(String[] args) {
+        try {
+            //Initialization
+			Alice aes = new Alice();
+            aes.init();
+			SecretKey key = aes.getKey();
+			IvParameterSpec iv = aes.getIV();
+			
+			//Accept Plaintext from User
+			Scanner myReader = new Scanner(System.in);
+			System.out.println("Enter plaintext to encrypt: ");
+			String plaintext = myReader.nextLine();
+			myReader.close();
+			
+			//Encryption Process
+            String encryptedMessage = aes.encrypt(plaintext, key, iv);
+			System.out.println("");
+			System.out.println("Encrypted Ciphertext: " + encryptedMessage);
+			
+			//Save shared key into key.txt
+			String encodedKey = Base64.getEncoder().encodeToString(key.getEncoded());
+			FileWriter myWriter1 = new FileWriter("key.txt");
+			myWriter1.write(encodedKey);
+			myWriter1.close();
+			
+			//Save IV into IV.txt
+			String encodedIV = Base64.getEncoder().encodeToString(iv.getIV());
+			FileWriter myWriter2 = new FileWriter("IV.txt");
+			myWriter2.write(encodedIV);
+			myWriter2.close();
+			
+			//Save encrpyted ciphertext into ctext.txt
+			FileWriter myWriter3 = new FileWriter("ctext.txt");
+			myWriter3.write(encryptedMessage);
+			myWriter3.close();
+			
+        } catch (Exception ignored) {
+        }
+    }
+}
---
+++ 2022 Programs/Information Security Encryption Exercises/AES Encryption Exercise/Bob.java
@@ -0,0 +1,116 @@
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileWriter;
+
+import javax.crypto.Cipher;
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+import javax.crypto.spec.IvParameterSpec;
+import java.security.SecureRandom;
+import java.util.Base64;
+
+
+public class Bob{
+    //Initialize Global Variables
+	private SecretKey Key;
+    private IvParameterSpec IV;
+	private final int KEY_SIZE = 128;
+    private Cipher decryptionCipher;
+	
+	
+	//Initialize AES Class
+    public void init() throws Exception {
+        //Generate Key
+		KeyGenerator key_generator = KeyGenerator.getInstance("AES");
+        key_generator.init(KEY_SIZE);
+        Key = key_generator.generateKey();
+		
+		//Generate IV
+		byte[] iv = new byte[16];
+		new SecureRandom().nextBytes(iv);
+		IV = new IvParameterSpec(iv);
+    }
+	
+	public SecretKey getKey() {
+		return Key;
+	}
+	
+	public IvParameterSpec getIV() {
+		return IV;
+	}
+	
+	//Decryption
+    //**********************************************************************************
+	public String decrypt(String encryptedMessage, SecretKey key, IvParameterSpec iv) throws Exception {
+        //Create byte array of ciphertext
+		byte[] messageInBytes = decode(encryptedMessage);
+        
+		//Create AES decryption cipher
+		decryptionCipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+        
+		//Initialize AES decryption cipher with generated key and IV
+		decryptionCipher.init(Cipher.DECRYPT_MODE, key, iv);
+        
+		//Create decrypted message
+		byte[] decryptedBytes = decryptionCipher.doFinal(messageInBytes);
+        
+		//Return decrypted message
+		return new String(decryptedBytes);
+    }
+	
+	//Convert decrypted message from String to byte array
+    private byte[] decode(String data) {
+        return Base64.getDecoder().decode(data);
+    }
+	//**********************************************************************************
+
+    public static void main(String[] args) {
+        try {
+            //Initialization
+			Bob aes = new Bob();
+            aes.init();
+			
+			//Retrieve shared key from key.txt
+			File keytext = new File("key.txt");
+			Scanner myReader1 = new Scanner(keytext);
+			String encodedkey = "";
+			while (myReader1.hasNextLine()) {
+				encodedkey = myReader1.nextLine();	
+			}
+			myReader1.close();
+			byte[] decodedkey = Base64.getDecoder().decode(encodedkey);
+			SecretKey key = new SecretKeySpec(decodedkey, 0, decodedkey.length, "AES");
+			
+			//Retrieve IV from IV.txt
+			File IVtext = new File("IV.txt");
+			Scanner myReader2 = new Scanner(IVtext);
+			String encodedIV = "";
+			while (myReader2.hasNextLine()) {
+				encodedIV = myReader2.nextLine();	
+			}
+			myReader2.close();
+			byte[] decodedIV = Base64.getDecoder().decode(encodedIV);
+			IvParameterSpec iv = new IvParameterSpec(decodedIV);
+			
+			//Retrieve ciphertext from ctext.txt
+			File ctext = new File("ctext.txt");
+			Scanner myReader3 = new Scanner(ctext);
+			String cyphertext = "";
+			while (myReader3.hasNextLine()) {
+				cyphertext = myReader3.nextLine();	
+			}
+			myReader3.close();
+			
+			System.out.println("");
+			System.out.println("Ciphertext:  " + cyphertext);
+			
+			//Decryption Process
+			String decryptedMessage = aes.decrypt(cyphertext, key, iv);
+            System.out.println("");
+			System.out.println("Decrypted Plaintext:  " + decryptedMessage);
+			
+        } catch (Exception ignored) {
+        }
+    }
+}
