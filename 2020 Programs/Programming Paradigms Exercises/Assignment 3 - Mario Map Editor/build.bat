dab32f45b84fa7a505b03cef5e26171bc6372fe3
Rudy Ramirez <77511166+rlramire10@users.noreply.github.com>
2022-03-24 16:57:19 -0500

    Delete 2020 Programs/Programming Paradigms Exercises/Assignment 3 - Mario Map Editor directory

--- 2020 Programs/Programming Paradigms Exercises/Assignment 3 - Mario Map Editor/build.bat
+++
@@ -1,9 +0,0 @@
-::@echo off
-javac Game.java View.java Controller.java Model.java Tube.java Json.java
-if %errorlevel% neq 0 (
-	echo There was an error; exiting now.	
-) else (
-	echo Compiled correctly!  Running Game...
-	java Game	
-)
-
